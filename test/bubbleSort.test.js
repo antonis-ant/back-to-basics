afa9b3a53b994991b588279529bb8f2839815924
Antonis Antoniadis <antonis.ant2011@gmail.com>
2023-11-18 17:21:17 +0200

    Separate testing for each algorithm

---
+++ test/bubbleSort.test.js
@@ -0,0 +1,12 @@
+const { bubbleSort } = require('../src/sorts');
+
+describe("Bubble Sort Test Cases", () => {
+    const list = [8, 1, 14, 0, 4, 63, 7, 33, 9, 10, 99];
+    const sortedList = [...list].sort((a, b) => a - b);
+
+    test("Bubble Sort", () => {
+        const res = bubbleSort([...list]);
+
+        expect(res).toEqual(sortedList);
+    });
+});
---
+++ test/insertionSort.test.js
@@ -0,0 +1,12 @@
+const { insertionSort } = require('../src/sorts');
+
+describe("Insertion Sort Test Cases", () => {
+    const list = [8, 1, 14, 0, 4, 63, 7, 33, 9, 10, 99];
+    const sortedList = [...list].sort((a, b) => a - b);
+
+    test("Insertion Sort", () => {
+        const res = insertionSort([...list]);
+
+        expect(res).toEqual(sortedList);
+    });
+});
